cd92cf73847120e1f0c5413c7b4c3420725a0342
Duncan MacDonald <duncmac.16@gmail.com>
2020-06-28 11:55:03 -0700

    Added comments to the photon mapping code

--- source/materials/material.cpp
+++ source/materials/material.cpp
@@ -3,7 +3,7 @@
 namespace poly::material {
 
 	void poly::material::Material::bounce_photon(structures::Photon &photon, std::vector <poly::structures::Photon> &photons,
-		unsigned int max_depth, std::vector <std::shared_ptr<poly::object::Object>> scene, float intensity) const {
+		unsigned int max_depth, std::vector <std::shared_ptr<poly::object::Object>> scene, float object_colour_intensity) const {
 		poly::structures::SurfaceInteraction si;
 		atlas::math::Ray<atlas::math::Vector> photon_ray = photon.reflect_ray();
 		bool is_hit{false};
@@ -15,10 +15,10 @@ namespace poly::material {
 
 		if (is_hit) {
 			poly::structures::Photon reflected_photon = poly::structures::Photon(photon_ray,
-				 si.hitpoint_get(), si.m_normal, photon.intensity() * (1 - intensity), photon.depth() + 1);
+				si.hitpoint_get(), si.m_normal, photon.intensity() * (1 - object_colour_intensity), photon.depth() + 1);
 			si.m_material->absorb_photon(reflected_photon, photons, max_depth, scene);
 		}
-		float new_intensity = photon.intensity() * intensity;
+		float new_intensity = photon.intensity() * object_colour_intensity;
 		photon.intensity(new_intensity);
 	}
 }
